bf5e43b0084fa0beb0644d4724672c77bc102de8
Léo Bonhême--Morsent <gturyz@gmail.com>
2022-06-02 11:01:53 +0200

    exo 2

---
+++ src/components/Compteur.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { ref } from "vue";
+
+const compteur = ref(0);
+const handleReset = () => {
+  compteur.value = 0;
+};
+</script>
+
+<template>
+  <input type="number" name="compteur" v-model="compteur" />
+  <p>Mon compteur : {{ compteur }}</p>
+  <button @click="handleReset">Reset</button>
+</template>
+
+<style scoped></style>
